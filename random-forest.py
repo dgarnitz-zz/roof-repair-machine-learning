a08c62c1d4f45ce795186771f027421d9fec6ada
dag8 <dag8@pc2-023-l.cs.st-andrews.ac.uk>
2019-06-21 18:38:28 +0100

    plot of the residuals is working, can only plot one feature against the output at a time, and since many of the features are one hot encoded, it does not make sense to do this for very many features

--- random-forest.py
+++ random-forest.py
@@ -1,13 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
 import pickle
-import seaborn as sns
-import matplotlib.pyplot as plt
+
 
 #load data
 data = pd.read_excel('./regression-training-data/PriceData.xlsx', sheet_name='SeasonalData', header=None)
@@ -20,7 +19,7 @@ y = data.iloc[1:,0]
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 100) 
 
 #initialize the model
-linreg = LinearRegression()
+rf = RandomForestRegressor(n_estimators = 100, criterion='mae')
 
 #standardize the data
 scaler = StandardScaler()
@@ -29,10 +28,10 @@ x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
 #train the model
-linreg.fit(x_train, y_train)
+rf.fit(x_train, y_train)
 
 #test generalization on a new dataset
-y_prediction = linreg.predict(x_test)
+y_prediction = rf.predict(x_test)
 print("MSE: ")
 print (mean_squared_error(y_test, y_prediction))
 print("MAE: ")
@@ -41,18 +40,5 @@ print("R2 of the model: ")
 print(r2_score(y_test, y_prediction))
 
 #cross validate with 5 fold CV
-cv = cross_validate(linreg, x_train, y_train, cv=5, return_train_score=False)
+cv = cross_validate(rf, x_train, y_train, cv=5, return_train_score=False, return_estimator=True)
 print(cv)
-
-#Residual Plot --> check for normality, residuals should be balanced on each side, ideally normally distributed
-                    # want to underestimate as often as you overestimate
-                    # want to overestimate by the same amount as you underestimate
-                    # does not make sense to do this for one hot encoded data
-
-sns.set(style="whitegrid")
-sns.residplot(x_train[:, 0], y_train, lowess=True, color="g")
-plt.show()
-
-# save the model to disk
-# with open('../API/price-predict/lin-reg.sav', 'wb') as path:
-#     pickle.dump(linreg, path)
